e4f82777092af64265c6c868ad1dbd92f36bd76d
Damaris Poppe <70035439+dpoppe7@users.noreply.github.com>
2024-08-07 22:46:23 -0600

    Add static background image on right side of landing page

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Landing.css';
+import backgroundImage from '../assets/Untitled 8.png'; 
 import Square_yellowcolors from '../assets/square_yellowcolors.png';
 import Square_bluecolors from '../assets/square_bluecolors.png';
 import Square_pink from '../assets/square_pink.png';
@@ -24,6 +25,7 @@ import Trig_orange from '../assets/trig_orange.png';
 import Trig_yellow from '../assets/trig_yellow.png';
 import Trig_dkgreen from '../assets/trig_dkgreen.png';
 
+
 // Defining all 'shapes' types:
 /* 
   TL - Top Left
@@ -33,92 +35,150 @@ import Trig_dkgreen from '../assets/trig_dkgreen.png';
   C - Center
 */
 const shapes = [
-  { id: 1, type: 'TLSquare_yellowcolors', image: Square_yellowcolors, top: '20%', left: '15%', width: '4rem' },
-  { id: 1, type: 'TLSquare_bluecolors', image: Square_bluecolors, top: '7%', left: '10%', width: '4rem' },
-  { id: 2, type: 'TLSquare_pink', image: Square_pink, top: '26%', left: '26%', width: '3rem' },
-  { id: 2, type: 'TLSquare_orange1', image: Square_orange, top: '3%', left: '1%', width: '3rem' },
-  { id: 2, type: 'TLSquare_orange2', image: Square_orange, top: '4%', left: '30%', width: '3.5rem', rotate: '-45deg' },
-  { id: 2, type: 'TLRect_green', image: Rect_green, top: '30%', left: '9%', width: '6rem' },
-  { id: 2, type: 'TLRect_small_yellow1', image: Rect_small_yellow, top: '37%', left: '3%', width: '1.5rem', height: '5rem', rotate: '90deg' },
-  { id: 2, type: 'TLRect_small_yellow2', image: Rect_small_yellow, top: '20%', left: '6%', width: '2rem', height: '5rem', rotate: '90deg' },
-  { id: 2, type: 'TLRect_small_yellow3', image: Rect_small_yellow, top: '-3%', left: '26%', width: '2rem', height: '4rem', rotate: '90deg' },
-  { id: 2, type: 'TLRect_small_pink', image: Rect_small_pink, top: '1%', left: '6.5%', width: '1.5rem', height: '4rem' },
-  { id: 2, type: 'TLRect_small_pink2', image: Rect_small_pink, top: '31%', left: '21%', width: '1.5rem', height: '4rem', rotate: '90deg' },
-  { id: 2, type: 'TLRect_small_blue', image: Rect_small_blue, top: '12%', left: '23%', width: '4.4rem', height: '2rem'},
-  { id: 2, type: 'TLRound_blue', image: Round_blue, top: '32%', left: '-1%', width: '3.2rem' },
-  { id: 2, type: 'TLRound_yellow', image: Round_yellow, top: '15%', left: '8%', width: '3rem' },
-  { id: 2, type: 'TLRound_yellow2', image: Round_yellow, top: '19%', left: '31%', width: '3rem' },
-  { id: 2, type: 'TLRound_pink1', image: Round_pink, top: '-1%', left: '18%', width: '4.5rem' },
-  { id: 2, type: 'TLTrig_blue', image: Trig_blue, top: '32%', left: '4.5%', width: '2.5rem', rotate: '-90deg' },
-  { id: 2, type: 'TLTrig_orange', image: Trig_orange, top: '15%', left: '3%', width: '2.5rem' },
-  { id: 2, type: 'TLTrig_yellow', image: Trig_yellow, top: '11%', left: '18%', width: '2rem' },
-  { id: 2, type: 'TLTrig_dkgreen', image: Trig_dkgreen, top: '26%', left: '22.5%', width: '2rem' },
-  { id: 2, type: 'TLRound_group_brown', image: Round_group_brown, top: '21%', left: '-3%', width: '5.5rem', rotate: '90deg' },
-  { id: 2, type: 'TLRound_group_purple', image: Round_group_purple, top: '-4%', left: '13%', width: '1.5rem', rotate: '90deg' },
-  { id: 2, type: 'TLRound_group_green', image: Round_group_green, top: '13%', left: '25%', width: '1.5rem', rotate: '90deg' },
+  { id: 1, type: 'TLSquare_yellowcolors', image: Square_yellowcolors, originalTop: '20%', originalLeft: '15%', width: '4rem' },
+  { id: 1, type: 'TLSquare_bluecolors', image: Square_bluecolors, originalTop: '7%', originalLeft: '10%', width: '4rem' },
+  { id: 2, type: 'TLSquare_pink', image: Square_pink, originalTop: '26%', originalLeft: '26%', width: '3rem' },
+  { id: 2, type: 'TLSquare_orange1', image: Square_orange, originalTop: '3%', originalLeft: '1%', width: '3rem' },
+  { id: 2, type: 'TLSquare_orange2', image: Square_orange, originalTop: '4%', originalLeft: '30%', width: '3.5rem', rotate: '-45deg' },
+  { id: 2, type: 'TLRect_green', image: Rect_green, originalTop: '30%', originalLeft: '9%', width: '6rem' },
+  { id: 2, type: 'TLRect_small_yellow1', image: Rect_small_yellow, originalTop: '37%', originalLeft: '3%', width: '1.5rem', height: '5rem', rotate: '90deg' },
+  { id: 2, type: 'TLRect_small_yellow2', image: Rect_small_yellow, originalTop: '20%', originalLeft: '6%', width: '2rem', height: '5rem', rotate: '90deg' },
+  { id: 2, type: 'TLRect_small_yellow3', image: Rect_small_yellow, originalTop: '-3%', originalLeft: '26%', width: '2rem', height: '4rem', rotate: '90deg' },
+  { id: 2, type: 'TLRect_small_pink', image: Rect_small_pink, originalTop: '1%', originalLeft: '6.5%', width: '1.5rem', height: '4rem' },
+  { id: 2, type: 'TLRect_small_pink2', image: Rect_small_pink, originalTop: '31%', originalLeft: '21%', width: '1.5rem', height: '4rem', rotate: '90deg' },
+  { id: 2, type: 'TLRect_small_blue', image: Rect_small_blue, originalTop: '12%', originalLeft: '23%', width: '4.4rem', height: '2rem'},
+  { id: 2, type: 'TLRound_blue', image: Round_blue, originalTop: '32%', originalLeft: '-1%', width: '3.2rem' },
+  { id: 2, type: 'TLRound_yellow', image: Round_yellow, originalTop: '15%', originalLeft: '8%', width: '3rem' },
+  { id: 2, type: 'TLRound_yellow2', image: Round_yellow, originalTop: '19%', originalLeft: '31%', width: '3rem' },
+  { id: 2, type: 'TLRound_pink1', image: Round_pink, originalTop: '-1%', originalLeft: '18%', width: '4.5rem' },
+  { id: 2, type: 'TLTrig_blue', image: Trig_blue, originalTop: '32%', originalLeft: '4.5%', width: '2.5rem', rotate: '-90deg' },
+  { id: 2, type: 'TLTrig_orange', image: Trig_orange, originalTop: '15%', originalLeft: '3%', width: '2.5rem' },
+  { id: 2, type: 'TLTrig_yellow', image: Trig_yellow, originalTop: '11%', originalLeft: '18%', width: '2rem' },
+  { id: 2, type: 'TLTrig_dkgreen', image: Trig_dkgreen, originalTop: '26%', originalLeft: '22.5%', width: '2rem' },
+  { id: 2, type: 'TLRound_group_brown', image: Round_group_brown, originalTop: '21%', originalLeft: '-3%', width: '5.5rem', rotate: '90deg' },
+  { id: 2, type: 'TLRound_group_purple', image: Round_group_purple, originalTop: '-4%', originalLeft: '13%', width: '1.5rem', rotate: '90deg' },
+  { id: 2, type: 'TLRound_group_green', image: Round_group_green, originalTop: '13%', originalLeft: '25%', width: '1.5rem', rotate: '90deg' },
 
-  { id: 1, type: 'BLSquare_yellowcolors_rotated', image: Square_yellowcolors, top: '85%', left: '18%', width: '3rem', rotate: '45deg' },
-  { id: 2, type: 'BLSquare_orange', image: Square_orange, top: '54%', left: '15%', width: '3.6rem' },
-  { id: 2, type: 'BLSquare_pink', image: Square_pink, top: '81%', left: '-0.5%', width: '4rem', rotate: '45deg' },
-  { id: 2, type: 'BLSquare_green', image: Square_green, top: '48%', left: '-1%', width: '3.6rem' },
-  { id: 2, type: 'BLRect_orange', image: Rect_orange, top: '67%', left: '10%', width: '5.5rem' },
-  { id: 2, type: 'BLRound_yellow', image: Round_yellow, top: '65%', left: '0.5%', width: '4rem' },
-  { id: 2, type: 'BLRound_blue', image: Round_blue, top: '89%', left: '8%', width: '3.2rem' },
-  { id: 2, type: 'BLRound_blue2', image: Round_blue, top: '43%', left: '18%', width: '3.2rem' },
-  { id: 2, type: 'BLRound_pink', image: Round_pink, top: '89%', left: '22%', width: '4rem' },
-  { id: 2, type: 'BLRound_pink2', image: Round_pink, top: '44%', left: '7%', width: '4.1rem' },
-  { id: 2, type: 'BLRound_group_green', image: Round_group_green, top: '63%', left: '21%', width: '1.5rem' },
-  { id: 2, type: 'BLRound_group_purple', image: Round_group_purple, top: '49%', left: '5%', width: '1.5rem' },
-  { id: 2, type: 'BLRound_group_brown', image: Round_group_brown, top: '82%', left: '9%', width: '5.5rem' },
-  { id: 2, type: 'BLRect_small_blue', image: Rect_small_blue, top: '76%', left: '5%', width: '4rem' },
-  { id: 2, type: 'BLRect_small_yellow', image: Rect_small_yellow, top: '88%', left: '14%', width: '2rem' },
-  { id: 2, type: 'BLTrig_green', image: Trig_green, top: '94%', left: '3%', width: '3rem' },
-  { id: 2, type: 'BLTrig_blue', image: Trig_blue, top: '76%', left: '18%', width: '2.5rem' },
-  { id: 2, type: 'BLTrig_lightblue', image: Trig_lightblue, top: '56%', left: '9%', width: '2.5rem' },
-  { id: 2, type: 'BLTrig_red', image: Trig_red, top: '54%', left: '21%', width: '2.5rem' },
+  { id: 1, type: 'BLSquare_yellowcolors_rotated', image: Square_yellowcolors, originalTop: '85%', originalLeft: '18%', width: '3rem', rotate: '45deg' },
+  { id: 2, type: 'BLSquare_orange', image: Square_orange, originalTop: '54%', originalLeft: '15%', width: '3.6rem' },
+  { id: 2, type: 'BLSquare_pink', image: Square_pink, originalTop: '81%', originalLeft: '-0.5%', width: '4rem', rotate: '45deg' },
+  { id: 2, type: 'BLSquare_green', image: Square_green, originalTop: '48%', originalLeft: '-1%', width: '3.6rem' },
+  { id: 2, type: 'BLRect_orange', image: Rect_orange, originalTop: '67%', originalLeft: '10%', width: '5.5rem' },
+  { id: 2, type: 'BLRound_yellow', image: Round_yellow, originalTop: '65%', originalLeft: '0.5%', width: '4rem' },
+  { id: 2, type: 'BLRound_blue', image: Round_blue, originalTop: '89%', originalLeft: '8%', width: '3.2rem' },
+  { id: 2, type: 'BLRound_blue2', image: Round_blue, originalTop: '43%', originalLeft: '18%', width: '3.2rem' },
+  { id: 2, type: 'BLRound_pink', image: Round_pink, originalTop: '89%', originalLeft: '22%', width: '4rem' },
+  { id: 2, type: 'BLRound_pink2', image: Round_pink, originalTop: '44%', originalLeft: '7%', width: '4.1rem' },
+  { id: 2, type: 'BLRound_group_green', image: Round_group_green, originalTop: '63%', originalLeft: '21%', width: '1.5rem' },
+  { id: 2, type: 'BLRound_group_purple', image: Round_group_purple, originalTop: '49%', originalLeft: '5%', width: '1.5rem' },
+  { id: 2, type: 'BLRound_group_brown', image: Round_group_brown, originalTop: '82%', originalLeft: '9%', width: '5.5rem' },
+  { id: 2, type: 'BLRect_small_blue', image: Rect_small_blue, originalTop: '76%', originalLeft: '5%', width: '4rem' },
+  { id: 2, type: 'BLRect_small_yellow', image: Rect_small_yellow, originalTop: '88%', originalLeft: '14%', width: '2rem' },
+  { id: 2, type: 'BLTrig_green', image: Trig_green, originalTop: '94%', originalLeft: '3%', width: '3rem' },
+  { id: 2, type: 'BLTrig_blue', image: Trig_blue, originalTop: '76%', originalLeft: '18%', width: '2.5rem' },
+  { id: 2, type: 'BLTrig_lightblue', image: Trig_lightblue, originalTop: '56%', originalLeft: '9%', width: '2.5rem' },
+  { id: 2, type: 'BLTrig_red', image: Trig_red, originalTop: '54%', originalLeft: '21%', width: '2.5rem' },
 
-  { id: 1, type: 'CSquare_yellowcolors', image: Square_yellowcolors, top: '12%', left: '69%', width: '3.7rem'},
-  { id: 1, type: 'CSquare_bluecolors', image: Square_bluecolors, top: '10%', left: '37%', width: '4rem' },
-  { id: 2, type: 'CSquare_pink', image: Square_pink, top: '13.5%', left: '44.5%', width: '2.5rem', rotate: '-45deg' },
-  { id: 2, type: 'CSquare_green', image: Square_green, top: '-2%', left: '64.5%', width: '4.8rem', rotate: '45deg' },
-  { id: 2, type: 'CRound_yellow', image: Round_yellow, top: '16%', left: '49%', width: '4rem' },
-  { id: 2, type: 'CRound_blue', image: Round_blue, top: '1%', left: '41%', width: '3rem' },
-  { id: 2, type: 'CRound_blue2', image: Round_blue, top: '23.5%', left: '65%', width: '3rem' },
+  { id: 1, type: 'CSquare_yellowcolors', image: Square_yellowcolors, originalTop: '12%', originalLeft: '69%', width: '3.7rem'},
+  { id: 1, type: 'CSquare_bluecolors', image: Square_bluecolors, originalTop: '10%', originalLeft: '37%', width: '4rem' },
+  { id: 2, type: 'CSquare_pink', image: Square_pink, originalTop: '13.5%', originalLeft: '44.5%', width: '2.5rem', rotate: '-45deg' },
+  { id: 2, type: 'CSquare_green', image: Square_green, originalTop: '-2%', originalLeft: '64.5%', width: '4.8rem', rotate: '45deg' },
+  { id: 2, type: 'CRound_yellow', image: Round_yellow, originalTop: '16%', originalLeft: '49%', width: '4rem' },
+  { id: 2, type: 'CRound_blue', image: Round_blue, originalTop: '1%', originalLeft: '41%', width: '3rem' },
+  { id: 2, type: 'CRound_blue2', image: Round_blue, originalTop: '23.5%', originalLeft: '65%', width: '3rem' },
 
-  { id: 2, type: 'CRound_pink', image: Round_pink, top: '-0.5%', left: '58%', width: '4.7rem' },
-  { id: 2, type: 'CTrig_green', image: Trig_green, top: '1%', left: '35.8%', width: '3rem', rotate: '-90deg' },
-  { id: 2, type: 'CTrig_orange', image: Trig_orange, top: '20.5%', left: '42.5%', width: '2.5rem' },
-  { id: 2, type: 'CTrig_yellow', image: Trig_yellow, top: '14%', left: '61%', width: '2rem' },
+  { id: 2, type: 'CRound_pink', image: Round_pink, originalTop: '-0.5%', originalLeft: '58%', width: '4.7rem' },
+  { id: 2, type: 'CTrig_green', image: Trig_green, originalTop: '1%', originalLeft: '35.8%', width: '3rem', rotate: '-90deg' },
+  { id: 2, type: 'CTrig_orange', image: Trig_orange, originalTop: '20.5%', originalLeft: '42.5%', width: '2.5rem' },
+  { id: 2, type: 'CTrig_yellow', image: Trig_yellow, originalTop: '14%', originalLeft: '61%', width: '2rem' },
 
-  { id: 2, type: 'CRect_green', image: Rect_green, top: '-3%', left: '46%', width: '6rem', rotate: '-45deg' },
-  { id: 2, type: 'CRect_small_yellow1', image: Rect_small_yellow, top: '13%', left: '65%', width: '1.5rem' },
-  { id: 2, type: 'CRect_small_blue', image: Rect_small_blue, top: '12%', left: '54%', width: '4rem', height: '1.4rem' },
-  { id: 2, type: 'CRound_group_brown', image: Round_group_brown, top: '1%', left: '53%', width: '5.5rem', rotate: '90deg' },
-  { id: 2, type: 'CRound_group_green', image: Round_group_green, top: '16%', left: '59%', width: '1.5rem', rotate: '90deg' },
-  { id: 2, type: 'CRound_group_purple', image: Round_group_purple, top: '19.8%', left: '73.5%', width: '1.5rem', rotate: '90deg' },
+  { id: 2, type: 'CRect_green', image: Rect_green, originalTop: '-3%', originalLeft: '46%', width: '6rem', rotate: '-45deg' },
+  { id: 2, type: 'CRect_small_yellow1', image: Rect_small_yellow, originalTop: '13%', originalLeft: '65%', width: '1.5rem' },
+  { id: 2, type: 'CRect_small_blue', image: Rect_small_blue, originalTop: '12%', originalLeft: '54%', width: '4rem', height: '1.4rem' },
+  { id: 2, type: 'CRound_group_brown', image: Round_group_brown, originalTop: '1%', originalLeft: '53%', width: '5.5rem', rotate: '90deg' },
+  { id: 2, type: 'CRound_group_green', image: Round_group_green, originalTop: '16%', originalLeft: '59%', width: '1.5rem', rotate: '90deg' },
+  { id: 2, type: 'CRound_group_purple', image: Round_group_purple, originalTop: '19.8%', originalLeft: '73.5%', width: '1.5rem', rotate: '90deg' },
 
 
 ];
 
 const Landing = () => {
+  const [shapePositions, setShapePositions] = useState(shapes);
+  const [blurPosition, setBlurPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = useCallback((e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    setBlurPosition({ x: mouseX, y: mouseY });
+
+    const updatedShapes = shapePositions.map((shape) => {
+      const shapeX = (parseFloat(shape.originalLeft) / 100) * window.innerWidth;
+      const shapeY = (parseFloat(shape.originalTop) / 100) * window.innerHeight;
+
+      const dx = shapeX - mouseX;
+      const dy = shapeY - mouseY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 150) { // Adjust the distance threshold as needed
+        const forceDirectionX = dx / distance;
+        const forceDirectionY = dy / distance;
+        const forceMagnitude = Math.min(500 / distance, 20); // Adjust force as needed
+
+        const newLeft = ((shapeX + forceDirectionX * forceMagnitude) / window.innerWidth) * 100;
+        const newTop = ((shapeY + forceDirectionY * forceMagnitude) / window.innerHeight) * 100;
+
+        return {
+          ...shape,
+          left: `${newLeft}%`,
+          top: `${newTop}%`,
+          rotation: shape.rotate || 0, // Ensure rotation is applied correctly
+        };
+      } else {
+        return {
+          ...shape,
+          left: shape.originalLeft,
+          top: shape.originalTop,
+          rotation: shape.rotate || 0, // Ensure rotation is applied correctly
+        };
+      }
+    });
+
+    setShapePositions(updatedShapes);
+  }, [shapePositions]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [handleMouseMove]);
+
   return (
     <div className="Landing">
-      <div className="Shapes-container">
-        {shapes.map((shape) => (
-          <img
-            key={shape.id}
-            src={shape.image}
-            alt={shape.type}
-            className={`Shape-img ${shape.type}`}
-            style={{ top: shape.top, left: shape.left, width: shape.width, height: shape.height, rotate:shape.rotate }}
-          />
-        ))}
+      <div
+        className="blur-circle"
+        style={{ top: `${blurPosition.y}px`, left: `${blurPosition.x}px` }}
+      />
+      <div>
+        <img src={backgroundImage} alt='Backgroun img' className="background-image"/>
       </div>
-      {/* <div className="Horizontal-line"></div>
-      <div className="Vertical-line"></div> */}
-      <header className="Landing-header">
-        Shapes
-      </header>
+      {shapePositions.map((shape) => (
+        <img
+          key={shape.id}
+          src={shape.image}
+          alt={shape.type}
+          className={`Shape-img ${shape.type}`}
+          style={{ 
+            top: shape.top, 
+            left: shape.left, 
+            width: shape.width, 
+            height: shape.height, 
+            transform: `rotate(${shape.rotation || 0})` // Apply rotation here
+          }}
+        />
+      ))}
     </div>
   );
 }
